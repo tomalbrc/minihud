40fb0bbfd1c998860ff21d39ea1aa178ca08e5ce
tomalbrc <tomalbrc@me.com>
2023-05-14 15:34:07 +0200

    1.20-pre1

--- src/main/java/fi/dy/masa/minihud/renderer/RenderObjectVbo.java
+++ src/main/java/fi/dy/masa/minihud/renderer/RenderObjectVbo.java
@@ -20,7 +20,7 @@ public class RenderObjectVbo extends RenderObjectBase
     {
         super(glMode, shader);
 
-        this.vertexBuffer = new VertexBuffer(VertexBuffer.class_8555.STATIC);
+        this.vertexBuffer = new VertexBuffer(VertexBuffer.Usage.STATIC);
         this.format = format;
 
         boolean hasTexture = false;
